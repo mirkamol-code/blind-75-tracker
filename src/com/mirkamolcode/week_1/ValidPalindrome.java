e1e10049aaaf2d79cee47d3e1feb96a9c904ea03
Mirkamol <mirkamol.m.a@gmail.com>
2026-03-12 02:45:51 +0500

    added a new solution for ValidPalindrome

--- src/com/mirkamolcode/week_1/ValidPalindrome.java
+++ src/com/mirkamolcode/week_1/ValidPalindrome.java
@@ -1,7 +1,12 @@
 package com.mirkamolcode.week_1;
 
 public class ValidPalindrome {
-    public boolean isPalindrome(String s) {
+    /**
+     * @Time complexity: O(n)
+     * @Space complexity: O(n)
+     * @param s
+     */
+    public boolean isPalindromeByReversing(String s) {
         StringBuilder newStr = new StringBuilder();
         for (char c : s.toCharArray()) {
             if (Character.isLetterOrDigit(c)) {
@@ -9,5 +14,34 @@ public class ValidPalindrome {
             }
         }
         return newStr.toString().equals(newStr.reverse().toString());
+    }
+
+    /**
+     * @Time complexity: O(n)
+     * @Space complexity: O(1)
+     * @param s
+     */
+    public boolean isPalindromeByTwoPointers(String s) {
+        int l = 0, r = s.length() - 1;
+
+        while (l < r) {
+            while (l < r && !alphaNum(s.charAt(l))) {
+                l++;
+            }
+            while (r > l && !alphaNum(s.charAt(r))) {
+                r--;
+            }
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+                return false;
+            }
+            l++; r--;
+        }
+        return true;
+    }
+
+    private boolean alphaNum(char c) {
+        return (c >= 'A' && c <= 'Z' ||
+                c >= 'a' && c <= 'z' ||
+                c >= '0' && c <= '9');
     }
 }
