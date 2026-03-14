f9139229222e2df512ee7967368ab8fb56e70726
Mirkamol <mirkamol.m.a@gmail.com>
2026-03-14 21:40:48 +0500

    add a solution for BinarySearch

---
+++ src/com/mirkamolcode/week_1/BinarySearchAlgo.java
@@ -0,0 +1,29 @@
+package com.mirkamolcode.week_1;
+
+public class BinarySearchAlgo {
+    /**
+     * @Time complexity: O(log n)
+     * @Space complexity: O(1)
+     * @param nums
+     * @param target
+     */
+    public int searchByIterative(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while(low <= high){
+            var middleOfArray = (low+high) / 2;
+            var middleNumber = nums[middleOfArray];
+
+            if(target == middleNumber){
+                return middleOfArray;
+            }
+            if(target < middleNumber){
+                high = middleOfArray - 1;
+            }else{
+                low = middleOfArray + 1;
+            }
+        }
+        return -1;
+    }
+}
